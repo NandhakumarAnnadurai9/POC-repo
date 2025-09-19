8ff68d9592f834f48608b7244d61b8bd67ff3734
NandhakumarAnnadurai <nandhakumar.annadurai@ctg.com>
2025-09-19 13:02:20 +0530

    Removed endpoint

--- sample_node/api/server.js
+++ sample_node/api/server.js
@@ -28,7 +28,7 @@ const createMockUser = (id, summaryText, detailText) => ({
 });
 
 // GET endpoint to generate a large list of data (~10MB)
-app.get('/api/data', (req, res) => {
+app.get('/', (req, res) => {
   console.log('Received request to generate 10MB of data...');
 
   const TARGET_SIZE_BYTES = 10 * 1024 * 1024; // 10 MB
